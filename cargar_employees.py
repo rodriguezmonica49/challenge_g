0102527c502b385372ae0c90b42b5eb2ac4ba9e2
Monica Rodriguez <rodriguezmonica49@gmail.com>
2024-03-05 22:08:51 -0500

    con RestApi

--- cargar_employees.py
+++ cargar_employees.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-
-
 def read_csv(file_name,columns):
     CSV_URL=f"C:/Users/Asus/OneDrive/Documentos/Entrevistas/Globant/Challenge_DA/{file_name}.csv"
 
@@ -9,12 +7,3 @@ def read_csv(file_name,columns):
     df_pandas = pd.read_csv(CSV_URL, sep=',',usecols=columns)
 
     return df_pandas
-
-#def insert_sql(df, name_table):
-
-
-df_employees = read_csv("hired_employees",['index','campo1','campo2','campo3','campo4'])
-#df_departments = read_csv("departments",[])
-#df_jobs = read_csv("jobs",[])
-
-
---
+++ main.py
@@ -0,0 +1,30 @@
+from typing import List, Union
+from connection import engine
+from fastapi import FastAPI
+from cargar_employees import read_csv
+app = FastAPI()
+
+from pydantic import BaseModel
+
+
+class DataSerializer(BaseModel):
+    filename: str
+    nametable: str
+    columns: List[str]
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/load_data")
+def load_data(data: DataSerializer):
+    df_employees = read_csv(data.filename, data.columns)
+    df_employees.to_sql(data.nametable, engine, chunksize=419,index=False, method='multi',if_exists='replace')
+
+    return {"result": "Data cargada"}
