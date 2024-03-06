c4a3c7f538ade806f8424084f9f2e3eaedff1383
Monica Rodriguez <rodriguezmonica49@gmail.com>
2024-03-06 14:35:43 -0500

    Ajuste eliminar cuaqluier Null

--- cargar_employees.py
+++ cargar_employees.py
@@ -1,9 +1,16 @@
 import pandas as pd
+import numpy as np
 
-def read_csv(file_name,columns):
+def read_csv(file_name):
     CSV_URL=f"C:/Users/Asus/OneDrive/Documentos/Entrevistas/Globant/Challenge_DA/{file_name}.csv"
 
     # Leer CSV con pandas
-    df_pandas = pd.read_csv(CSV_URL, sep=',',usecols=columns)
+    df_pandas = pd.read_csv(CSV_URL, sep=',', header = None)
 
     return df_pandas
+
+def clean_data(df):
+    #nan_rows = df[df.isnull().any(1)]
+    df = df.fillna(value=np.nan)
+    df = df.dropna()
+    return df
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import List, Union
 from connection import engine
 from fastapi import FastAPI
-from cargar_employees import read_csv
+from cargar_employees import read_csv, clean_data
 app = FastAPI()
 
 from pydantic import BaseModel
@@ -10,7 +10,7 @@ from pydantic import BaseModel
 class DataSerializer(BaseModel):
     filename: str
     nametable: str
-    columns: List[str]
+    #columns: List[str]
 
 
 @app.get("/")
@@ -24,7 +24,13 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.post("/load_data")
 def load_data(data: DataSerializer):
-    df_employees = read_csv(data.filename, data.columns)
-    df_employees.to_sql(data.nametable, engine, chunksize=419,index=False, method='multi',if_exists='replace')
+    df_employees = read_csv(data.filename)
+    df_employees = clean_data(df_employees)
+
+    df_employees.to_sql(data.nametable, engine, chunksize=419,index=False, method='multi', if_exists='replace')
 
     return {"result": "Data cargada"}
+
+df_employees = read_csv("jobs")
+df_employees = clean_data(df_employees)
+df_employees.to_sql("jobs", engine, chunksize=419,index=False, method='multi', if_exists='replace')
